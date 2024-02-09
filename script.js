6b9274066cd13197cd238e03c324debba586d8b2
wanbings <145480532+wanbings@users.noreply.github.com>
2024-02-09 12:43:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//main html
+let cost = 0;
+var riser; //declares all the necessary parts
+var limbs;
+var sight;
+var longrod;
+var siderods;
+var vbar;
+
+function updateCost(){
+    cost=99.99;
+    setTimeout(()=> {window.location.href='result.html'}, 5000);
+}
